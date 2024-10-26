b9a7823ff57f7e46bf8aa153250b674d0dfc3eb3
bishalr0y <bishalroy895@gmail.com>
2024-10-26 21:01:44 +0530

    add more test cases for fizzBuzz🐛

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -27,9 +27,19 @@ describe("max", () => {
 })
 
 describe("fizzBuzz", () => {
-    test("returns Fizz if divided by 3, Buzz if divided by 5 and FizzBuzz if divided by both ", () => {
-        expect(fizzBuzz(90)).toBe("FizzBuzz")
-        expect(fizzBuzz(10)).toBe("Buzz")
+    test("returns Fizz if the number is divisible by 3", () => {
         expect(fizzBuzz(39)).toBe("Fizz")
+    })
+
+    test("returns Buzz if the number is divisible by 5", () => {
+        expect(fizzBuzz(50)).toBe("Buzz")
+    })
+
+    test("returns FizzBuzz if the number is divisible by both 3 and 5", () => {
+        expect(fizzBuzz(90)).toBe("FizzBuzz")
+    })
+
+    test("returns the number itself if it is not divisible by 3 or 5", () => {
+        expect(fizzBuzz(91)).toBe("91")
     })
 })
